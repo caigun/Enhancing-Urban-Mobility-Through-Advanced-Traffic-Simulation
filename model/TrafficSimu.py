15c89a810bec57623b0eb09bb26345d9b722133f
yutengzhang03 <yutengzhang03@gmail.com>
2023-10-20 23:41:09 -0700

    Update TrafficSimu.py

--- model/TrafficSimu.py
+++ model/TrafficSimu.py
@@ -8,9 +8,9 @@ import util
 Structure of nodes: nodes <- util.Counter(traffic lights)
 nodes[any traffic light 1 with successor 2 3 4 5]: 
 {
-    Policy: {2->1: time_interval[0, 20], 3->1: time_interval[20, 40], 4->1: time_interval[40, 60], 5->1: time_interval[60, 80]}
+    Policy: {timeIntervals:[(2->1,time=20),(3->1,time=20),(4->1,time=20),(5->1,time=20)], timeForOneIteration:80}
     Queues: {queue of 2->1: [Times for cars to be at intersection], queue of 3->1: [Times for cars to be in intersection],...}
-    Transition function:{2:P(1->2), 3:P(1->3),..., disappear:P(disappear)}
+    Transition probability: 1/(# of successor + 1(disappear))
 }
 """
 def rdSegmentDis(roads):
@@ -25,7 +25,7 @@ def rdSegmentDis(roads):
         dis[(nodes[1],nodes[0])] = roads.l2Distance(point1,point2)*111194
     return dis
 
-def initialCars(nodes, roads, rdSegDis, divisor=50):
+def initialCars(nodes, roads, rdSegDis, divisor):
     """
     Return the intial state of traffic, including the number of cars on each Segment of roads.
     """
@@ -34,37 +34,38 @@ def initialCars(nodes, roads, rdSegDis, divisor=50):
         nodes[node]["Queues"] = dict()
         successors = roads.getSuccessors(node)
         for succ in successors:
-            nodes[node]["Queues"][(succ,node)] = [0] * (rdSegDis[(succ,node)]//divisor)
+            nodes[node]["Queues"][(succ,node)] = [0] * int(rdSegDis[(succ,node)]/divisor)
 
-def assignPolicy(nodes, roads, timePolicy=[0,20,40,60,80]):
+def assignPolicy(nodes, roads, timePolicy):
     """
     Assign policy to each traffic light.
     """
     for node in roads.nodes:
         nodes[node]["Policy"] = dict()
-        successors = roads.getSuccessors(node)
+        successors = list(roads.getSuccessors(node))
+        timeIntervals = []
         for i in range(len(successors)):
-            nodes[node]["Policy"][(successors[i],node)] = [timePolicy[i],timePolicy[i+1]]
+            timeIntervals.append((successors[i],timePolicy[i]))
+        nodes[node]["Policy"]["timeIntervals"] = timeIntervals
+        nodes[node]["Policy"]["timeForOneIteration"] = sum(timePolicy[:len(successors)])
 
-def createTrans(nodes, roads, transProb = None):
+def createTrans(nodes, roads, transPolicy):
     """
     Create the transition funtion to each successor of every traffic light
     P(1->2),P(1->3),P(1->4),P(1->5),P(1->disappear)
     """
-    if transProb == None:
+    if transPolicy == None:
         for node in roads.nodes:
             nodes[node]["Policy"] = util.Counter()
             successors = roads.getSuccessors(node)
-            l = len(successors)
-            for succ in successors:
-                nodes[node]["Policy"][(node,succ)] = 1/l
+            nodes[node]["TransProb"] = 1/(len(successors)+1)
     else:
         pass
 
-def initialize(nodes, roads, rdSegDis, divisor=50, timePolicy=[0,20,40,60,80], transProb = None):
-    initialCars(nodes, roads, rdSegDis, divisor=50)
-    assignPolicy(nodes, roads, timePolicy=[0,20,40,60,80])
-    createTrans(nodes, roads, transProb = None)
+def initialize(nodes, roads, rdSegDis, divisor=50, timePolicy=[20,]*6, transPolicy = None):
+    initialCars(nodes, roads, rdSegDis, divisor)
+    assignPolicy(nodes, roads, timePolicy)
+    createTrans(nodes, roads, transPolicy)
 
 def updatePolicy(nodes, results, alpha):
     """
@@ -83,6 +84,7 @@ def updateQueues(nodes, updateTime, time):
     """
     update the queues: pop cars that are out, push new cars in
     """
+
     pass
 
 def updateTimeLists(nodes, updateTime, time):
@@ -112,7 +114,7 @@ if __name__ == '__main__':
     rdSegDis = rdSegmentDis(roads)
 
     nodes = util.Counter()
-    initialize(nodes, roads, rdSegDis, divisor=50, timePolicy=[0,20,40,60,80], transProb = None)
+    initialize(nodes, roads, rdSegDis, divisor=50, timePolicy=[20,]*6, transPolicy = None)
     
     while time:
         time -= updateTime
